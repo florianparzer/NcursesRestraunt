78f0895690093665458a98de70a990f6c0dc82ea
Florian Parzer <ic20b081@technikum-wien.at>
2021-05-13 19:02:09 +0200

    main erstellt

---
+++ reservation.c
@@ -0,0 +1,18 @@
+/*
+ * reservation.c
+ *
+ *  Created on: May 13, 2021
+ *      Author: osboxes
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "resFunctions.h"
+
+int main(int argc, char **argv) {
+	char *name;
+	if(argc == 1){
+
+	}
+}
