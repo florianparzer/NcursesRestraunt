8197dd3a45dfe5194846b740f6c593f9a2eea562
Florian Parzer <ic20b081@technikum-wien.at>
2021-05-14 00:05:26 +0200

    ncursis anfang

--- reservation.c
+++ reservation.c
@@ -8,11 +8,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ncurses.h>
 #include "resFunctions.h"
 
-int main(int argc, char **argv) {
-	char *name;
+int main(int argc, char *argv[]) {
+	char file[20];
 	if(argc == 1){
-
+		scanf("%s", file);
+	}else{
+		strcpy(file,argv[1]);
 	}
+
+	initscr();
+	noecho();
+
+	box(stdscr, '*', '*');
+	move(10, 10);
+	hline('*', COLS/2);
+	vline('#', LINES/2);
+	mvprintw(15, (COLS/2)-10, file);
+	refresh();
+	getch();
+
+	echo();
+	endwin();
 }
